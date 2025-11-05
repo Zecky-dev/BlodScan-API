e8b383418765fdc93749c86298106f4955c5fbb8
Zecky-dev <ceng.zekeriyadonmez@gmail.com>
2025-11-05 16:27:42 +0300

    Add mock implementations for DOMMatrix, ImageData, and Path2D

--- services/parseService.js
+++ services/parseService.js
@@ -1,3 +1,17 @@
+if (typeof global.DOMMatrix === 'undefined') {
+  global.DOMMatrix = class MockDOMMatrix {};
+}
+
+if (typeof global.ImageData === 'undefined') {
+  global.ImageData = class MockImageData {
+    constructor(data, width, height) { /* constructor beklenirse */ }
+  };
+}
+
+if (typeof global.Path2D === 'undefined') {
+  global.Path2D = class MockPath2D {};
+}
+
 import { PDFParse } from "pdf-parse";
 import { cleanPdfText, extractResults } from "../utils/parseUtils.js";
 
